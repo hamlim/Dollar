aff8652bab17f685252d17077dd3f06de149c7c2
hamlim <hamlim@rpi.edu>
2016-01-28 18:22:50 -0500

    some changes

--- js/settings.js
+++ js/settings.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
         var user = JSON.parse(localStorage.getItem('user'));
         var username = user.username;
         //relavent elements
-        var userGreeting = document.getElementById('username-heading').innerHTML = "Hello "+username+"!";
+        var userGreeting = document.getElementById('username-heading');
+        userGreeting.innerHTML = "Hello "+username+"!";
         //account settings elements
         var passReset = document.getElementById('password-reset'),
         usernameReset = document.getElementById('username-reset'),
@@ -17,7 +18,13 @@ $(document).ready(function(){
             var newUName = usernameReset.value;
             if(newUName.value != ""){
                 //we know newUName is new
-                
+                if(newUName != username ){
+                    //we want to updat the local and the server versions of the user
+                    userGreeting.innerHTML = newUName;
+                    user.username = newUName;
+                    //now we need to update the server
+                    
+                }
             }
         }
         //handle settings
