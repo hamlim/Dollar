7b82e0171bc9c630196a7a85ae9e60e10438ba60
hamlim <hamlim@rpi.edu>
2016-01-28 18:16:23 -0500

    adding settings page

--- js/settings.js
+++ js/settings.js
@@ -8,8 +8,19 @@ $(document).ready(function(){
         var username = user.username;
         //relavent elements
         var userGreeting = document.getElementById('username-heading').innerHTML = "Hello "+username+"!";
-
+        //account settings elements
+        var passReset = document.getElementById('password-reset'),
+        usernameReset = document.getElementById('username-reset'),
+        accountSbmt = document.getElementById('account-btn');
+        accountSbmt.onclick = function(){
+            var newPass = passReset.value;
+            var newUName = usernameReset.value;
+            if(newUName.value != ""){
+                //we know newUName is new
+                
+            }
+        }
         //handle settings
-        
+
     }
 })
