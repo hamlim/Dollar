eee0a25bdbdbff2c12e912c01089869b74712547
hamlim <hamlim@rpi.edu>
2016-01-28 19:01:21 -0500

    adding tag update support

--- js/settings.js
+++ js/settings.js
@@ -84,6 +84,95 @@ $(document).ready(function(){
             }
         }
         //handle settings
+        //relevent elements
+        var appSbmt = document.getElementById('app-btn'),
+        tagOptionOne = document.getElementById('tag-option-one'),
+        tagOptionTwo = document.getElementById('tag-option-two'),
+        tagOptionThree = document.getElementById('tag-option-three');
+        appSbmt.onclick = function(){
+            var tagone = tagOptionOne.value;
+            var tagtwo = tagOptionTwo.value;
+            var tagthree = tagOptionThree.value;
+            if(tagone.length != 0){
+                if(tagtwo.length != 0){
+                    if(tagthree.length != 0){
+                        var tagstring = tagone+", "+tagtwo+", "+tagthree;
+                        var data = {
+                            "u_tags": tagstring
+                        };
+                        var packageData = {
+                            "fields": data
+                        };
+                        var settings = {
+                            "async": true,
+                            "crossDomain": true,
+                            "url": "https://api.airtable.com/v0/applYClUOdBXhRzGf/Users/"+userKey,
+                            "method": "PATCH",
+                            "headers": {
+                                "authorization": "Bearer keyIye3zskPSBMQ6Q",
+                                "content-type": "application/json"
+                            },
+                            "processData": false,
+                            "data": packageData
+                        }
 
+                        $.ajax(settings).done(function (response) {
+                            console.log(response);
+                            notie.alert(1, "Successfully updated your Tags!", 3);
+                        });
+                    } else {
+                        var tagstring = tagone+", "+tagtwo+", N/A";
+                        var data = {
+                            "u_tags": tagstring
+                        };
+                        var packageData = {
+                            "fields": data
+                        };
+                        var settings = {
+                            "async": true,
+                            "crossDomain": true,
+                            "url": "https://api.airtable.com/v0/applYClUOdBXhRzGf/Users/"+userKey,
+                            "method": "PATCH",
+                            "headers": {
+                                "authorization": "Bearer keyIye3zskPSBMQ6Q",
+                                "content-type": "application/json"
+                            },
+                            "processData": false,
+                            "data": packageData
+                        }
+
+                        $.ajax(settings).done(function (response) {
+                            console.log(response);
+                            notie.alert(1, "Successfully updated your Tags!", 3);
+                        });
+                    }
+                } else {
+                    var tagstring = tagone + ", N/A, N/A";
+                    var data = {
+                        "u_tags": tagstring
+                    };
+                    var packageData = {
+                        "fields": data
+                    };
+                    var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://api.airtable.com/v0/applYClUOdBXhRzGf/Users/"+userKey,
+                        "method": "PATCH",
+                        "headers": {
+                            "authorization": "Bearer keyIye3zskPSBMQ6Q",
+                            "content-type": "application/json"
+                        },
+                        "processData": false,
+                        "data": packageData
+                    }
+
+                    $.ajax(settings).done(function (response) {
+                        console.log(response);
+                        notie.alert(1, "Successfully updated your Tags!", 3);
+                    });
+                }
+            }
+        }
     }
 })
