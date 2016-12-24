39707fb54339b713ce95a5a71e363687c9e5a230
Matt Hamlin <hamlim@outlook.com>
2016-12-23 20:18:41 -0800

    Adding classnames package and some dynamic styles on the Link component

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+
 
 import Styles from './link.css';
+
+let cx = classNames.bind(Styles);
 
 // @TODO import classnames package as cx, to append an active class if props.isActive
 
 
 const Link = (props) => {
+  let className = cx({
+    'Link': props.default,
+    'Link is-active': props.active
+  });
   return (
-    <a href={props.href} className={Styles.Link}>
+    <a href={props.href} className={className}>
       {children}
     </a>
   )
