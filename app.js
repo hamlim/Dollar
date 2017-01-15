2efda64d03d0a73c412bf7e523494c88d1f1c920
Matt Hamlin <hamlim@outlook.com>
2017-01-15 16:01:44 -0500

    Adding some default stuff, not moving forward on this at the moment

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './src/components/header/header';
 import Footer from './src/components/footer/footer';
+import {Router, Route, Link, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 
 require('./src/styles.css');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      transactions: [],
+      budgets: [],
+      settings: {}
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <Footer />
+      </div>
+    )
+  }
+};
+
+
+
+render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="analysis" component={Analysis} />
+      <Route path="budgets" component={Budgets} />
+      <Route path="Settings" component={Settings} />
+    </Route>
+  </Router>,
+  document.getElementById('mount');
+)
 
 
 if (document.body.getAttribute('data-page') === "app") {
