50e86831419c2f362429f2c641262abe96d0f7f9
Matt Hamlin <hamlim@outlook.com>
2017-02-27 18:50:32 -0500

    getting started with redux and react router

--- src/start.js
+++ src/start.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 import Home from './components/home';
+import Analysis from './components/analysis';
+import Budgets from './components/budgets';
+import Transactions from './components/transactions';
+import Settings from './components/settings';
 
 import './start.css';
 
+import reducers from '/reducers';
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 const Dollar = _ => (
-  <Router history={browserHistory}>
-    <Route path="/" component={Home} />
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Home}/>
+      <Route path="/analysis" component={Analysis}/>
+      <Route path="/budgets" component={Budgets}/>
+      <Route path="/transactions" component={Transactions}/>
+      <Route path="/settings" component={Settings}/>
+    </Router>
+  </Provider>
 );
 
 export default Dollar;
